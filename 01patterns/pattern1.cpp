75ce4cdb49780e6b78f5fb4045c609c5876ed22e
susmita022 <85818236+susmita022@users.noreply.github.com>
2023-03-04 00:17:35 +0530

    Create pattern1.cpp

---
+++ 01patterns/pattern1.cpp
@@ -0,0 +1,13 @@
+      int i;
+        while( i!=s.size()){
+           
+            if(s==goal){
+                return 1;
+            }
+            else {
+                s=shift(s);
+               
+                }
+                 i++;
+        }
+        return 0;
